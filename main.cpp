94786ea642d383c1f470fa7ec53496e21c3d6081
Dovahstrun <43002140+Dovahstrun@users.noreply.github.com>
2018-09-05 15:48:09 +0100

    Window!
    It now opens a window called Button Masher

--- main.cpp
+++ main.cpp
@@ -1,4 +1,22 @@
+//Included libraries
+#include <sfml/Graphics.hpp>
+
+//Entry point for program
 int main()
 {
+	//Make a variable called gameWindow of type renderWindow
+	sf::RenderWindow gameWindow;
+	gameWindow.create(sf::VideoMode(800, 600), "Button Masher");
+
+	//Game Loop
+	while (true)
+	{
+		//TODO: Check for input
+
+		//TODO: Update game state
+
+		//TODO: Draw graphics
+	}
+	//Exit point for program
 	return 0;
 }
