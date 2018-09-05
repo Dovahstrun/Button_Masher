6a80177a4e6daac6be59c9f343733c53043ab731
Dovahstrun <43002140+Dovahstrun@users.noreply.github.com>
2018-09-05 15:59:53 +0100

    Made the window fullscreen and unable to be maximised or resized

--- main.cpp
+++ main.cpp
@@ -6,16 +6,23 @@ int main()
 {
 	//Make a variable called gameWindow of type renderWindow
 	sf::RenderWindow gameWindow;
-	gameWindow.create(sf::VideoMode(800, 600), "Button Masher");
+	gameWindow.create(sf::VideoMode::getDesktopMode(), "Button Masher", sf::Style::Titlebar | sf::Style::Close);
 
 	//Game Loop
-	while (true)
+	while (gameWindow.isOpen()) //Runs every frame until the game window is closed
 	{
 		//TODO: Check for input
 		sf::Event gameEvent;
 		while (gameWindow.pollEvent(gameEvent))
 		{
-			//TODO: Process Events
+			//Process Events
+
+			//Check if the event is the closed event
+			if (gameEvent.type == sf::Event::Closed)
+			{
+				//Close the game window
+				gameWindow.close();
+			}
 		}
 
 		//TODO: Update game state
